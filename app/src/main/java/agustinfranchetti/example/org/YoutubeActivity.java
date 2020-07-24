f688d3dd5aee9d05cd552857bd408a450243e0f1
agustin <agustinfranchetti@gmail.com>
2020-07-24 16:02:43 -0300

    Problema con 2nd Activity

--- app/src/main/java/agustinfranchetti/example/org/YoutubeActivity.java
+++ app/src/main/java/agustinfranchetti/example/org/YoutubeActivity.java
@@ -1,19 +1,34 @@
 package agustinfranchetti.example.org;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+       // setContentView(R.layout.activity_youtube);
+        //ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+        /*Button button1 = new Button(this);
+        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
+        button1.setText("button added");
+        layout.addView(button1);*/
+        YouTubePlayerView player = new YouTubePlayerView(this);
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(player);
     }
 
     @Override
