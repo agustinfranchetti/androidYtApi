3c710b6f82e78aa2f8261af62ef07ae4b7fad67e
agustin <agustinfranchetti@gmail.com>
2020-07-24 14:32:52 -0300

    added YoutubeActivity.Java - added yt.jar to Gradle

--- app/src/main/java/agustinfranchetti/example/org/MainActivity.java
+++ app/src/main/java/agustinfranchetti/example/org/MainActivity.java
@@ -11,9 +11,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-<<<<<<< HEAD
-=======
-    //comentario
-    //comentario ubuntu
->>>>>>> c3928a4259e5c4fbf0095841ce0b3734ca97c733
 }
---
+++ app/src/main/java/agustinfranchetti/example/org/YoutubeActivity.java
@@ -0,0 +1,28 @@
+package agustinfranchetti.example.org;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+}
