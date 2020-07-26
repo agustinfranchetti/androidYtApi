80f57c879b0679c951a4098a8f7b4c14660d63be
agustin <agustinfranchetti@gmail.com>
2020-07-26 12:10:19 -0300

    Agregados los OnClickListeners de SubmenuActivity

---
+++ app/src/main/java/agustinfranchetti/example/org/SubmenuActivity.java
@@ -0,0 +1,45 @@
+package agustinfranchetti.example.org;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubmenuActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submenu);
+
+        Button btnPlayVideo = (Button) findViewById(R.id.btnPlayVideo);
+        Button btnPlayList = (Button) findViewById(R.id.btnPlayList);
+
+        btnPlayVideo.setOnClickListener(this);
+        btnPlayList.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = null;
+        switch (view.getId()){
+            case R.id.btnPlayVideo:
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, YoutubeActivity.YT_APIP, YoutubeActivity.YT_VIDEO_ID);
+                break;
+            case R.id.btnPlayList:
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, YoutubeActivity.YT_APIP, YoutubeActivity.YT_PLAYLIST);
+                break;
+            default:
+        }
+
+        if (intent !=null){
+            startActivity(intent);
+        }
+
+    }
+}
