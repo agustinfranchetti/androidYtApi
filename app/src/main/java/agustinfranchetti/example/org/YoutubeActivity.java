886d3eafdbbda4dcb5c57378c2799f400d1b82fd
agustin <agustinfranchetti@gmail.com>
2020-07-26 00:23:48 -0300

    agregada fncinalidad de OnInitializationSuccess/Failure

--- app/src/main/java/agustinfranchetti/example/org/YoutubeActivity.java
+++ app/src/main/java/agustinfranchetti/example/org/YoutubeActivity.java
@@ -5,8 +5,10 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
@@ -15,6 +17,7 @@ import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
 
+    private static final String TAG = "YoutubeActivity";
     static final String YT_APIP = new String(String.valueOf(R.string.YT_API));
     static final String YT_VIDEO_ID = "WTsmIbNku5g";
     static final String YT_PLAYLIST = "PLOzDu-MXXLliO9fBNZOQTBDddoA3FzZUo";
@@ -37,15 +40,27 @@ public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlaye
         YouTubePlayerView playerView = new YouTubePlayerView(this);
         playerView.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
         layout.addView(playerView);
+        playerView.initialize(YT_APIP, this);
     }
 
     @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
+        Log.d(TAG, "onInitializationSuccess: provider is " + provider.getClass().toString());
+        Toast.makeText(this, "YT player inicializado correctamente", Toast.LENGTH_LONG).show();
 
+        if (wasRestored){
+            youTubePlayer.cueVideo(YT_VIDEO_ID);
+        }
     }
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
-
+        final int REQUEST_CODE = 1;
+        if(youTubeInitializationResult.isUserRecoverableError()){
+            youTubeInitializationResult.getErrorDialog(this,REQUEST_CODE).show();
+        }else{
+            String errorMessage = String.format("Error al Inicializar Reproductor (%1$s)", youTubeInitializationResult.toString());
+            Toast.makeText(this, errorMessage,Toast.LENGTH_LONG).show();
+        }
     }
 }
